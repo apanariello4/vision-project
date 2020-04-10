f4563547d575fc3c327b8472c670a0a6b8343ce1
apanariello4 <killetore@gmail.com>
2020-04-10 14:34:28 +0200

    read and write videos

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
-#ueue
-#eueu
-#oeeeeeeee
+import sys
+sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages') # in order to import cv2 under python3
+import cv2
+import numpy as np
 
-#FABRIZIO TEST COMMIT
-#si
-asdasd
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+cap = cv2.VideoCapture('videos/VIRB0391.MP4')
+
+frame_width = int(cap.get(3))
+frame_height = int(cap.get(4))
+
+codec = cv2.VideoWriter_fourcc('X','V','I','D')
+fps = 10
+out = cv2.VideoWriter('output.avi',codec, fps, (frame_width,frame_height))
+
+# Check if camera opened successfully
+if (cap.isOpened()== False): 
+  print("Error opening video stream or file")
+
+# Read until video is completed
+while(cap.isOpened()):
+  # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    if ret == True:
+        
+        frame = cv2.Canny(frame,100,200)
+        out.write(frame)
+        cv2.imshow('Frame',frame)
+
+        # Press Q on keyboard to  exit
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
+
+    # Break the loop
+    else: 
+        break
+
+# When everything done, release the video capture and writer objects
+cap.release()
+out.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
+
