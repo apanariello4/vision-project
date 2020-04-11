aaabb8237b45262a0ac6cfe37c8ac0404972c7d2
apanariello4 <killetore@gmail.com>
2020-04-11 15:05:23 +0200

    orb

--- main.py
+++ main.py
@@ -5,15 +5,15 @@ import numpy as np
 
 # Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
-cap = cv2.VideoCapture('videos/VIRB0392.MP4')
+cap = cv2.VideoCapture("videos/VIRB0391.MP4")
 
 frame_width = int(cap.get(3))
 frame_height = int(cap.get(4))
 
 codec = cv2.VideoWriter_fourcc(*"DIVX")
-fps = 30
+fps = 10
 out = cv2.VideoWriter('output.avi',0, fps, (frame_width,frame_height))
-og = True
+first_frame_flag = True
 
 # Check if camera opened successfully
 if (cap.isOpened()== False): 
@@ -23,7 +23,6 @@ if (cap.isOpened()== False):
 while(cap.isOpened()):
     # Capture frame-by-frame
     ret, frame = cap.read()
-    prev_img = None
 
     if ret == True:
 
@@ -36,20 +35,21 @@ while(cap.isOpened()):
         #---------------Saliency---------------------------------------
         # initialize OpenCV's static fine grained saliency detector and
         # compute the saliency map
-        saliency = cv2.saliency.StaticSaliencyFineGrained_create()
-        (success, saliencyMap) = saliency.computeSaliency(frame)
+        #saliency = cv2.saliency.StaticSaliencyFineGrained_create()
+        #(success, saliencyMap) = saliency.computeSaliency(frame)
         # if we would like a *binary* map that we could process for contours,
         # compute convex hull's, extract bounding boxes, etc., we can
         # additionally threshold the saliency map
 
-        threshMap = cv2.threshold(saliencyMap.astype("uint8"), 0, 255, cv2.THRESH_BINARY)[1]
+        #threshMap = cv2.threshold(saliencyMap.astype("uint8"), 0, 255, cv2.THRESH_BINARY)[1]
         #----------------------------------------------------------------
 
         grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) 
+        #gaussian = cv2.GaussianBlur(grayscale, (3, 3), 100)
         _,thresh = cv2.threshold(grayscale[:,:,2],10,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
         #kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (25,25))
-        #opening = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=3)
+        #opening = cv2.morpholfirst_frameyEx(thresh, cv2.MORPH_OPEN, kernel, iterations=3)
 
         contours, hierarchy = cv2.findContours(thresh,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
 
@@ -60,11 +60,35 @@ while(cap.isOpened()):
             # find the biggest countour (c) by the area
             c = max(contours, key = cv2.contourArea)
 
+            # determine the most extreme points along the contour
+            extLeft = tuple(c[c[:, :, 0].argmin()][0])
+            extRight = tuple(c[c[:, :, 0].argmax()][0])
+            extTop = tuple(c[c[:, :, 1].argmin()][0])
+            extBot = tuple(c[c[:, :, 1].argmax()][0])
+        
+            # cv2.circle(frame, extLeft, 8, (0, 0, 255), -1)
+            # cv2.circle(frame, extRight, 8, (0, 255, 0), -1)
+            # cv2.circle(frame, extTop, 8, (255, 0, 0), -1)
+            # cv2.circle(frame, extBot, 8, (255, 255, 0), -1)
+
             # approximate the contour (approx) with 10% tolerance
             epsilon = 0.1*cv2.arcLength(c,True)
             approx = cv2.approxPolyDP(c,epsilon,True)
             x,y,w,h = cv2.boundingRect(approx)
             
+
+            n = approx.ravel()  
+            i = 0
+            font = cv2.FONT_HERSHEY_COMPLEX 
+            current_bb = []
+            for j in n : 
+                if(i % 2 == 0): 
+                    _x = n[i] 
+                    _y = n[i + 1] 
+
+                    current_bb.append([_x,_y])
+                i += 1
+
             # draw the biggest contour (c) in green
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
 
@@ -72,19 +96,50 @@ while(cap.isOpened()):
             rect = cv2.minAreaRect(approx)
             box = cv2.boxPoints(rect)
             box = np.int0(box)
-            cv2.drawContours(frame,[box],0,(0,0,255),2)
+            #cv2.drawContours(frame,[box],0,(0,0,255),2)
 
-            if og:
-                og_points = np.float32([[x, y],[x+w, y+h],[x, y+h],[x+w, y]])
-                og = False
-        
-        current_bb = np.float32([[x, y],[x+w, y+h],[x, y+h],[x+w, y]])
+            #(x, y) upper left, (x+w, y) upper right, (x, y+h) bottom left, (x+w, y+h) bottom right
 
-        M = cv2.getPerspectiveTransform(og_points, current_bb)
+            if first_frame_flag:
+                first_frame_points = np.float32([[x, y],[x, y+h],[x+w, y+h],[x+w, y]])
+                first_frame_img = frame.copy()
+                first_frame_flag = False
+            
+
+        #current_bb = np.float32([[x, y],[x+w, y+h],[x, y+h],[x+w, y]])
+        if len(current_bb) == 4:
+            dst_bb = np.float32(current_bb)
+            prev_bb = np.float32(current_bb)
+        else:
+            dst_bb = prev_bb
+        cv2.circle(frame, tuple(dst_bb[0]), 8, (0, 0, 255), -1) #red - upper left
+        cv2.circle(frame, tuple(dst_bb[1]), 8, (0, 255, 0), -1) #green - bottom left
+        cv2.circle(frame, tuple(dst_bb[2]), 8, (255, 0, 0), -1) #blue - bottom right
+        cv2.circle(frame, tuple(dst_bb[3]), 8, (255, 255, 0), -1) #aqua - upper right
+
+        M, _ = cv2.findHomography(first_frame_points, np.float32(dst_bb))
         imageWarped = cv2.warpPerspective(frame, M, (frame_width, frame_height))
+
+        orb = cv2.ORB()
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = orb.detectAndCompute(first_frame_img,None)
+        kp2, des2 = orb.detectAndCompute(frame,None)
+
+        # create BFMatcher object
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+        # Match descriptors.
+        matches = bf.match(des1,des2)
+
+        # Sort them in the order of their distance.
+        matches = sorted(matches, key = lambda x:x.distance)
+
+        # Draw first 10 matches.
+        img3 = cv2.drawMatches(first_frame_img,kp1,frame,kp2,matches[:10], flags=2)
+
         #out.write(imageWarped)
-        cv2.imshow('Frame',frame)
-        cv2.imshow('Warped',imageWarped)
+        cv2.imshow('Frame',img3)
+        #cv2.imshow('Warped',imageWarped)
 
         # Press Q on keyboard to  exit
         if cv2.waitKey(25) & 0xFF == ord('q'):
