HTRDC finished but wrong result

htrdc.py
@@ -1,70 +1,72 @@
-"""
-# 1. compute center
-# 2. compute ru
-# 3. compute rd
-# 4. compute r
-
-#Parameters k_1: distortion parameter, k_min-k_max=[0,1] distortion range
-#c_x, c_y: center of distortion (0,0)
-#n: number of samples of k_1
-#eps: acceptable error on k_1
-
-#Canny
-
-#Apply Distortion for k_j^i
-
-# Compute HT
-
-# Apply Gaussian Filter to HT
-
-# Find the maximum M_j of HT_j
-
-# repeat for i<n
-"""
 import numpy as np
+
+from skimage.transform import hough_line
 
 
 def HTRDC(edges, range_k, n, epsilon):
     """
 
     :param edges: edges of image obtained with canny edge detector
-    :param range_k: tuple containg the min and max of the 
+    :param range_k: tuple containg the min and max of the
     distortion parameter
     :param n: number of samples of k_1
     :param epsilon: acceptable error
     :return:
     """
+    max_k = None
 
     h, w = edges.shape
     k_min, k_max = range_k
+
     center_x, center_y, coordinates = get_center_and_coordinates(h, w)
     center = np.array((center_x, center_y))
     ru = np.sqrt(np.sum(np.square(coordinates), axis=1))
+    theta = np.deg2rad(np.arange(start=0, stop=181, step=1))
 
-    rd = []
+    # poitns_u = coordinates + (coordinates - center) * (k * np.sum(coordinates)**2)
+
     while (k_max - k_min) > epsilon:
         step = (k_max - k_min) / n
         k_range = np.arange(start=k_min + step, stop=k_max, step=step)
+
+        if k_range.size < n:
+            k_range = np.hstack((k_range, np.array([k_max], dtype=np.float)))
+        rd = []
         for k in k_range:
-            poitns_u = coordinates + (coordinates - center) * (k * np.sum(coordinates)**2)
-            rd.append(compute_rd(ru, k))
+            rd_k = compute_rd(ru, k)
+            rd.append(rd_k)
+
         rd = np.array(rd, dtype=np.float)
         ru_copy = np.tile(ru, n).reshape(rd.shape)
         rs = rd / ru_copy
         ht_max = np.zeros(rs.shape[0], dtype=np.float)
-        for i in arange(rs.shape[0]):
-            undistorted = compute_undistorted()
+
+        for i in np.arange(rs.shape[0]):
+            undistorted = compute_undistorted(
+                edges, center_y, center_x, rs[i], coordinates)
+            acc, _, _ = hough_line(undistorted, theta)
+            ht_max[i] = np.max(acc)
+
+        argmax = np.argmax(ht_max)
+        max_k = k_range[argmax]
+        k_max = np.min([max_k + step, 1.])
+        k_min = np.max([0., max_k - step])
+        n = np.int(np.round(n * 1.1))
+
+    return max_k
 
 
 def compute_rd(ru, k):
     """
-
-    :param ru: sqrt(x_u^2 + y_u^2)
-    :param k: distortion coefficient
-    :return: rd
+    Compute rd following formula 6 of the paper.
+    :param ru:
+    :param k:
+    :return:
     """
-    rd_1 = np.cbrt(ru / 2*k + np.sqrt((1 / 3*k)**3 + (ru / 2*k)**2))
-    rd_2 = np.cbrt(ru / 2*k - np.sqrt((1 / 3*k)**3 + (ru / 2*k)**2))
+    x = ru / (2*k)
+    y = np.sqrt(np.power((1 / (3*k)), 3) + np.square(x))
+    rd_1 = np.cbrt(x + y)
+    rd_2 = np.cbrt(x - y)
 
     return rd_1 + rd_2
 
@@ -85,7 +87,35 @@ def get_center_and_coordinates(h, w):
     x, y = np.mgrid[-c_y:h - c_y, -c_x:w-c_x]
     points = np.vstack((x.ravel(), y.ravel())).T
 
+    points = np.delete(points, c_y*w+c_x, axis=0)
+
     return c_x, c_y, points
 
 
-def compute_undistorted()
+def compute_undistorted(img, cy, cx, rs, points):
+    """
+    Compute the 'undistorted' version of the image following formula 7
+    :param img:
+    :param cy:
+    :param cx:
+    :param rs:
+    :param points:
+    :return:
+    """
+    out = np.zeros_like(img)
+    rs = np.vstack((rs, rs)).T
+    distorted = np.multiply(points, rs)
+    distorted = np.round(distorted).astype(np.int)
+    out[points[:, 0] + cy, points[:, 1] +
+        cx] = img[distorted[:, 0] + cy, distorted[:, 1] + cx]
+    out[cy, cx] = img[cy, cx]
+    return out
+
+
+def undistort(img, k):
+    h, w = img.shape[0], img.shape[1]
+    cx, cy, points = get_center_and_coordinates(h, w)
+    ru = np.sqrt(np.sum(np.square(points), axis=1))
+    rd = compute_rd(ru, k)
+    r = rd / ru
+    return compute_undistorted(img, cy, cx, r, points)