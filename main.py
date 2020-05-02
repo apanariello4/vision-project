1e5468cd84e08342755e94e9420fcd43dc587175
apanariello4 <265925@studenti.unimore.it>
2020-05-02 10:56:15 +0200

    Moved functions to utils module

--- main.py
+++ main.py
@@ -15,29 +15,18 @@ HTRDC_N = 20
 HTRDC_EPSILON = 1e-6
 
 
-def resize_to_ratio(img, ratio):
-    """
-    Resize an image according to the given ration
-    :param img: Image to be resized
-    :param ratio: ratio used to resize the image
-    :return: Image resized
-    """
-    assert ratio > 0, 'ratio_percent must be > 0'
-    w = int(img.shape[1] * ratio)
-    h = int(img.shape[0] * ratio)
-    return cv2.resize(img, (w, h), interpolation=cv2.INTER_AREA)
+def compute_HTRDC(img):
 
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (3, 3), 15)
+    canny = painting_detection.auto_canny(gray)
+    print("Computing HTRDC")
+    if not k:  # Computes k only for the first frame
+        k = HTRDC(canny,
+                  (HTRDC_K_START, HTRDC_K_END), HTRDC_N, HTRDC_EPSILON)
+    print("Distortion Parameter: ", k)
 
-def resize_when_too_big(img, threshold_w_h):
-    h = int(img.shape[0])
-    w = int(img.shape[1])
-    thr_w, thr_h = threshold_w_h
-    if h > thr_h or w > thr_h:
-        h_ratio = thr_h / h
-        w_ratio = thr_w / w
-        ratio = min(h_ratio, w_ratio)
-        img = resize_to_ratio(img, ratio)
-    return img
+    return undistort(img, k)
 
 
 def main():
@@ -76,17 +65,7 @@ def main():
             #         frame.copy(), contours, approximate=False)
             # # img = resize_when_too_big(frame, (720, 405))
 
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            gray = cv2.GaussianBlur(gray, (3, 3), 15)
-            canny = painting_detection.auto_canny(gray)
-            print("Computing HTRDC")
-            if not k:  # Computes k only for the first frame
-                k = HTRDC(canny,
-                          (HTRDC_K_START, HTRDC_K_END), HTRDC_N, HTRDC_EPSILON)
-            print("Distortion Parameter: ", k)
-
-            frame_undistorted = undistort(frame, k)
-            cv2.imshow("Frame", frame_undistorted)
+            cv2.imshow("Frame", frame)
 
             # out.write(img3)
             # Press Q on keyboard to  exit
