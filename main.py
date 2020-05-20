e84a843892662b630184b8bc6763fd21827a91ce
apanariello4 <265925@studenti.unimore.it>
2020-05-20 12:15:12 +0200

    tests

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import numpy as np
 from cv2 import cv2
 from people_detection import DetectNet
 import painting_detection
-from ccl import *
+from ccl import image_segmentation, draw_components
 from htrdc import HTRDC, undistort
-from utils import resize_when_too_big
+from utils import resize_when_too_big, equalize_luma
 from darknet_yolo.darknet_pytorch import Darknet
 
 HTRDC_K_START = 0.0
@@ -44,7 +44,7 @@ def main():
     first_frame_flag = True
     k = None
 
-    net = Darknet()
+    # net = Darknet()
 
     # Check if camera opened successfully
     if cap.isOpened() == False:
@@ -56,23 +56,31 @@ def main():
         ret, frame = cap.read()
 
         if ret == True:
-            net.yolo_detection(frame)
-            hough_contours = painting_detection.hough_contours(frame.copy())
+           # net.yolo_detection(frame)
+            # img = resize_when_too_big(frame, (720, 405))
+            # img = erosion_dilation(img)
+            # contours, hierarchy = painting_detection.contours(
+            #     img, adaptive=True)
+
+            # if len(contours) != 0:
+            #     painting_detection.draw_contours(
+            #         img, contours, approximate=False)
+
+            # img2 = resize_when_too_big(frame, (720, 405))
+            img2 = frame.copy()
+
+            img_equalized = equalize_luma(img2)
             contours, hierarchy = painting_detection.contours(
-                frame.copy(), adaptive=False)
-
+                img_equalized, adaptive=True)
             if len(contours) != 0:
-                painting_detection.draw_contours(
-                    frame.copy(), contours, approximate=False)
+                painting_detection.draw_biggest_roi(
+                    img_equalized, contours, approximate=False)
 
-            img = resize_when_too_big(frame, (720, 405))
-            components = labeling(img)
-            drawn_components = draw_components(components)
+            # components, stats = image_segmentation(img_equalized)
+            # drawn_components = draw_components(components)
+            #painting_detection.draw_ccl_rois(img2, stats)
 
-            painting_detection.draw_contours(
-                drawn_components, contours, approximate=True)
-
-            cv2.imshow("Frame", drawn_components)
+            cv2.imshow("Frame", img_equalized)
 
             # out.write(img3)
             # Press Q on keyboard to  exit
