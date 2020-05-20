c3ca83bb2be7a160ba093439614bb0c0075db2ed
fabriziosorgente <f.sorgente1@gmail.com>
2020-05-20 13:23:54 +0200

    Fix to people localization and painting retrieval

--- people_localization.py
+++ people_localization.py
@@ -2,27 +2,55 @@ from painting_retrieval import RetrieveClass
 import cv2
 import pandas as pd
 import json
+import os
+from scripts.write_map_coordinates import write_json_coordinates
+import time
 
 
 class LocalizeClass:
 
-    def __init__(self, ranked_list):
-        self.paintings_ranked_list = ranked_list
+    def __init__(self):
+        self.paintings_ranked_list = {}
         self.museum_map = cv2.imread('resources/map.png')
         self.df = pd.read_csv('resources/data.csv')
         self.rect_color = (0, 0, 255)
         self.rect_thickness = 5
         self.room_coordinates = {}
         self.loaded_coordinates = False
+        self.load_room_coordinates()
         print("Ready for people localization")
         print("___________________________________")
 
-    def localize(self):
-
-        if not self.loaded_coordinates:
+    def load_room_coordinates(self) -> None:
+        """
+        It loads the room coordinates from a file. If the file is not in the specified path,
+        it runs a script to generate it
+        """
+        start = time.time()
+        print("Searching for room coordinates...", end="", flush=True)
+        if os.path.exists('resources/rooms_coordinates.json') and os.path.exists('resources/rooms_coordinates.json'):
+            print("[File found]")
             with open('resources/rooms_coordinates.json', 'r') as fp:
                 self.room_coordinates = json.load(fp)
+            self.loaded_coordinates = True
+        else:
+            print("[File not found, running script to generate the file]")
+            write_json_coordinates()
+            with open('resources/rooms_coordinates.json', 'r') as fp:
+                self.room_coordinates = json.load(fp)
+            self.loaded_coordinates = True
+        end = time.time()
+        print("[LOADING] Loading time: " + "%.4f" % (end - start) + " seconds")
 
+    def localize(self, ranked_list):
+        """
+        It takes a ranked list of paintings and determine the room in which the painting is in
+            :param ranked_list: ranked list of matches for each painting
+        """
+        if not self.loaded_coordinates:
+            self.load_room_coordinates()
+
+        self.paintings_ranked_list = ranked_list
         best_match = max(self.paintings_ranked_list, key=self.paintings_ranked_list.get)
         if self.paintings_ranked_list[best_match] > 0:
             room = self.df[self.df.Image == best_match[-7:]].Room.item()
@@ -31,8 +59,10 @@ class LocalizeClass:
             print('\nThe painting is in room ' + str(room))
             self.show_map(
                 cv2.rectangle(self.museum_map.copy(), start_point, end_point, self.rect_color, self.rect_thickness))
+            return room
         else:
-            print("Can't determine the room")
+            print("\nCan't determine the room")
+            return None
 
     def show_map(self, img):
         cv2.namedWindow("Map", cv2.WINDOW_KEEPRATIO)
