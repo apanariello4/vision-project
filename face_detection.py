40db3fafe8107a5b3d6dfe80f6fc6a598ad1313e
fabriziosorgente <f.sorgente1@gmail.com>
2020-05-24 12:47:29 +0200

    Fixed face detection

--- face_detection.py
+++ face_detection.py
@@ -14,26 +14,38 @@ class FaceDetectionClass():
 
     def is_facing_camera(self, person_roi) -> bool:
         start = time.time()
-
-        gray = cv2.cvtColor(person_roi, cv2.COLOR_BGR2GRAY)
-        face = self.face_cascade.detectMultiScale(gray, 1.3, 5)
-        # self.show_img(person_roi)
-        if face:
+        face = []
+        gray_person_roi = cv2.cvtColor(person_roi, cv2.COLOR_BGR2GRAY)
+        face = self.face_cascade.detectMultiScale(gray_person_roi, 1.3, 5)
+        if len(face) != 0:
+            self.draw_face(person_roi, face[0])
             (x, y, w, h) = face[0]
-            roi_gray = gray[y:y + h, x:x + w]
-            eyes = self.eye_cascade.detectMultiScale(roi_gray)
+            face_roi = gray_person_roi[y:y + h, x:x + w]
+            eyes = self.eye_cascade.detectMultiScale(face_roi)
             end = time.time()
             print("Time to detect the face: " + "%.2f" % (
                     end - start) + " seconds")
-            if eyes:
+            if len(eyes) != 0:
+                for (ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(person_roi, (ex + x, ey + y), (ex + ew + x, ey + eh + y), (0, 255, 0), 2)
+                self.show_img(person_roi)
                 return True
             else:
+                self.show_img(person_roi)
                 return False
         else:
             end = time.time()
             print("Time to detect the face: " + "%.2f" % (
                     end - start) + " seconds")
             return False
+
+    def draw_face(self, img, face):
+        (x, y, w, h) = face
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    def draw_eyes(self, img, eyes):
+        ex, ey, ew, eh = eyes
+        cv2.rectangle(img, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
     def detect_faces_and_draw(self, person_roi) -> None:
         gray = cv2.cvtColor(person_roi, cv2.COLOR_BGR2GRAY)
@@ -55,5 +67,5 @@ class FaceDetectionClass():
         cv2.namedWindow("Face detection", cv2.WINDOW_KEEPRATIO)
         cv2.imshow("Face detection", img)
         cv2.resizeWindow("Face detection", int(img.shape[1] / 2), int(img.shape[0] / 2))
-        cv2.waitKey(10)
+        cv2.waitKey(0)
         cv2.destroyAllWindows()
