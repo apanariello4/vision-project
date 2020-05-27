f6070cdfbc750cbd046d22349287f598692c7052
fabriziosorgente <f.sorgente1@gmail.com>
2020-05-27 15:54:12 +0200

    Added is_overlapping method to check the overlap of two rects and fixed debug strings

--- face_detection.py
+++ face_detection.py
@@ -12,7 +12,7 @@ class FaceDetectionClass():
         print("[INFO] Ready for face detection")
         print("___________________________________")
 
-    def is_facing_camera(self, person_roi) -> bool:
+    def is_facing_camera(self, person_roi, draw=False) -> bool:
         print("[INFO] Performing face detection")
 
         start = time.time()
@@ -20,16 +20,18 @@ class FaceDetectionClass():
         gray_person_roi = cv2.cvtColor(person_roi, cv2.COLOR_BGR2GRAY)
         face = self.face_cascade.detectMultiScale(gray_person_roi, 1.3, 5)
         if len(face) != 0:
-            self.draw_face(person_roi, face[0])
+            if draw:
+                self.draw_face(person_roi, face[0])
             (x, y, w, h) = face[0]
             face_roi = gray_person_roi[y:y + h, x:x + w]
             eyes = self.eye_cascade.detectMultiScale(face_roi)
             end = time.time()
-            print("[INFO] Face found. Time to detect the face: " + "%.2f" % (
+            print("[INFO] The person is facing the camera. Time to detect the face: " + "%.2f" % (
                     end - start) + " seconds")
             if len(eyes) != 0:
-                for (ex, ey, ew, eh) in eyes:
-                    cv2.rectangle(person_roi, (ex + x, ey + y), (ex + ew + x, ey + eh + y), (0, 255, 0), 2)
+                if draw:
+                    for (ex, ey, ew, eh) in eyes:
+                        cv2.rectangle(person_roi, (ex + x, ey + y), (ex + ew + x, ey + eh + y), (0, 255, 0), 2)
                 self.show_img(person_roi)
                 return True
             else:
@@ -37,9 +39,51 @@ class FaceDetectionClass():
                 return False
         else:
             end = time.time()
-            print("[INFO] No face found. Time to detect the face: " + "%.2f" % (
+            print("[INFO] The person is not facing the camera. Time to detect the face: " + "%.2f" % (
                     end - start) + " seconds")
             return False
+
+    def is_facing_paintings(self, person_detection, paintings_detections, draw=False):
+        if len(paintings_detections) <= 0:
+            print("[INFO] There are no paintings near the person")
+            return False
+        start = time.time()
+
+        bb_person_left = person_detection[2]
+        bb_person_right = person_detection[4]
+        bb_person_top = person_detection[3]
+        bb_person_bottom = person_detection[5]
+        person_rect = [bb_person_top, bb_person_left, bb_person_bottom, bb_person_right]
+        is_overlapping = False
+        for painting_detection in paintings_detections:
+            bb_painting_left = painting_detection[2]
+            bb_painting_right = painting_detection[4]
+            bb_painting_top = painting_detection[3]
+            bb_painting_bottom = painting_detection[5]
+            painting_rect = [bb_painting_top, bb_painting_left, bb_painting_bottom, bb_painting_right]
+            is_overlapping = self.is_overlapping(person_rect, painting_rect)
+            if is_overlapping:
+                break
+        end = time.time()
+        if is_overlapping:
+            print("[INFO] The person is facing at least one painting")
+            print("[INFO] Time to check if the person if facing a painting: " + "%.3f" % (
+                    end - start) + " seconds")
+            return True
+        else:
+            print("[INFO] The person is not facing any paintings")
+            print("[INFO] Time to check if the person if facing a painting: " + "%.3f" % (
+                    end - start) + " seconds")
+            return False
+
+    def is_overlapping(self, rect1, rect2):
+        min_y1, min_x1, max_y1, max_x1, = rect1
+        min_y2, min_x2, max_y2, max_x2 = rect2
+        if min_x1 > max_x2 or max_x1 < min_x2:
+            return False
+        if min_y1 > max_y2 or max_y1 < min_y2:
+            return False
+        return True
 
     def draw_face(self, img, face):
         (x, y, w, h) = face
@@ -69,5 +113,5 @@ class FaceDetectionClass():
         cv2.namedWindow("Face detection", cv2.WINDOW_KEEPRATIO)
         cv2.imshow("Face detection", img)
         cv2.resizeWindow("Face detection", int(img.shape[1] / 2), int(img.shape[0] / 2))
-        cv2.waitKey(0)
+        cv2.waitKey(10)
         cv2.destroyAllWindows()
